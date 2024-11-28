debc5b9de86ccd1959dfca45045fb54bc864d815
Ugo Wadoux <ugowadoux@MacBook-Pro-de-Ugo.local>
2024-11-28 14:11:36 +0100

    Init app

---
+++ iakadir_app/iakadir_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iakadir_appApp.swift
+//  iakadir_app
+//
+//  Created by Ugo Wadoux on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct iakadir_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iakadir_appTests/iakadir_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  iakadir_appTests.swift
+//  iakadir_appTests
+//
+//  Created by Ugo Wadoux on 28/11/2024.
+//
+
+import Testing
+@testable import iakadir_app
+
+struct iakadir_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
